14f5a627cdb757a3b1649c44973abc2240c36744
mohamed <kittanyenterprise@gmail.com>
2024-11-15 16:16:10 +0200

    dan side :)

--- billing/app/routes/__init__.py
+++ billing/app/routes/__init__.py
@@ -1,14 +1,14 @@
-# from .provider import provider_bp
+from .provider import get_providers
 # from .truck import truck_bp
 # from .rate import rate_bp
-from .health_check import health_check as health_check_bp
+from .health_check import health_check
 # from .bill import bill_bp
 
 def register_blueprints(app):
-    # app.register_blueprint(provider_bp)
+    app.register_blueprint(get_providers)
     # app.register_blueprint(truck_bp)
     # app.register_blueprint(rate_bp)
-    app.register_blueprint(health_check_bp)
+    app.register_blueprint(health_check)
     # app.register_blueprint(bill_bp)
 
 
--- billing/app/routes/provider.py
+++ billing/app/routes/provider.py
@@ -1,24 +1,24 @@
 from flask import Blueprint, jsonify, request
-#from app.extensions. import db
 from app.extensions import get_mysql_connection as db
 
 
 provider_bp = Blueprint('provider', __name__, url_prefix="/providers")
 
-# @provider_bp.route('/', methods=['GET'])
-# def get_providers():
-#     cursor = db.get_connection().cursor(dictionary=True)
-#     cursor.execute("SELECT * FROM Provider")
-#     result = cursor.fetchall()
-#     cursor.close()
-#     return jsonify(result)
+@provider_bp.route('/', methods=['GET'])
+def get_providers():
+    print("IM HERE")
+    cursor = db.get_connection().cursor(dictionary=True)
+    cursor.execute("SELECT * FROM Provider")
+    result = cursor.fetchall()
+    cursor.close()
+    return jsonify(result)
 
-# @provider_bp.route('/', methods=['POST'])
-# def create_provider():
-#     data = request.get_json()
-#     name = data.get("name")
-#     cursor = db.get_connection().cursor()
-#     cursor.execute("INSERT INTO Provider (name) VALUES (%s)", (name,))
-#     db.get_connection().commit()
-#     cursor.close()
-#     return jsonify({"message": "Provider created"}), 201
+@provider_bp.route('/', methods=['POST'])
+def create_provider():
+    data = request.get_json()
+    name = data.get("name")
+    cursor = db.get_connection().cursor()
+    cursor.execute("INSERT INTO Provider (name) VALUES (%s)", (name,))
+    db.get_connection().commit()
+    cursor.close()
+    return jsonify({"message": "Provider created"}), 201
