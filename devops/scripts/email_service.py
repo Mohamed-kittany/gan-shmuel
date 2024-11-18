b1cebd0d3bb9ae4be5856bd31a718a0067597486
AM8151 <markowits8151@gmail.com>
2024-11-18 03:35:56 -0600

    email update

--- devops/scripts/email_service.py
+++ devops/scripts/email_service.py
@@ -2,7 +2,8 @@ import smtplib
 from dotenv import load_dotenv
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from email.utils import formataddr
+from email.mime.base import MIMEBase
+from email import encoders
 import os
 from logging_config import logger
 
@@ -10,10 +11,13 @@ load_dotenv(dotenv_path=".env.test")
 
 
 def send_email(subject, body, to_addresses):
-    logger.info( os.getenv("EMAIL_USERNAME"))
+    logger.info(os.getenv("EMAIL_USERNAME"))
     """Send an email notification."""
     from_email = os.getenv("EMAIL_USERNAME")
     from_password = os.getenv("EMAIL_PASSWORD")
+
+    # Path to the log file
+    log_file_path = os.path.join(os.path.dirname(__file__), 'logs', 'ci_pipeline.log')
 
     # Set up the MIME
     msg = MIMEMultipart()
@@ -23,6 +27,17 @@ def send_email(subject, body, to_addresses):
 
     # Attach the body to the email with UTF-8 encoding
     msg.attach(MIMEText(body, 'plain', _charset='utf-8'))
+
+    # Attach the log file
+    try:
+        with open(log_file_path, "rb") as log_file:
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(log_file.read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', 'attachment', filename='ci_pipeline.log')
+            msg.attach(part)
+    except Exception as e:
+        logger.error(f"Failed to attach log file: {e}")
 
     # SMTP server configuration (e.g., Gmail)
     smtp_server = "smtp.gmail.com"
