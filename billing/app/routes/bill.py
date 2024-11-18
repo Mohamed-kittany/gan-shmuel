ce052390936a17222cf0802eaca1a90797af918d
daniel <danysir18@gmail.com>
2024-11-18 18:29:40 +0200

    started working on Bill

--- billing/app/routes/bill.py
+++ billing/app/routes/bill.py
@@ -1,3 +1,12 @@
-from flask import Blueprint, jsonify # type: ignore
+from flask import Blueprint, jsonify
+import logging
+from ..services import BillService
 
 billing_bp = Blueprint('bill', __name__)
+
+logger = logging.getLogger(__name__)
+
+@billing_bp.route("/bill/<id>?from=t1&to=t2", methods=["GET"])
+def get_bill(id, time_start, time_end):
+    logger.info("Received request to create a Bill.")
+    
