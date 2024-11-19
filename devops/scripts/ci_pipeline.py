bdf476d33a21d210b7188db01c6a9883c4482d74
AM8151 <markowits8151@gmail.com>
2024-11-19 23:28:33 +0000

    ci update

--- devops/scripts/ci_pipeline.py
+++ devops/scripts/ci_pipeline.py
@@ -1002,7 +1002,7 @@ class CIPipeline:
         self.repo_dir = self.base_dir / "gan-shmuel"
         
         # Initialize managers
-        self.repo_manager = RepositoryManager(repo_url, self.repo_dir, self.logger)
+        self.repo_manager = RepositoryManager(repo_url, self.repo_dir)
         self.docker_manager = DockerManager(self.logger)
 
     def load_environment(self, env_file: str) -> str:
