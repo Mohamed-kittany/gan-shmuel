879af9a8084e12da424046f919178f7730204010
MishaTitov <michaeltitov1@gmail.com>
2024-11-20 11:59:38 +0200

    fixed: fixes in seesion/id in result from qeury

--- weight/routes/get_session.py
+++ weight/routes/get_session.py
@@ -13,20 +13,20 @@ def get_session(id):
         return jsonify({"error": "Session not found"}), 404
 
     response = {
-        "id": result['session_id'],
-        "truck": result['truck'],
-        "bruto": result['bruto'],
-        "produce": result['produce']  # Include produce in the response
+        "session_id": result[9],
+        "truck": result[3],
+        "bruto": result[5],
+        "produce": result[8]  # Include produce in the response
     }
 
-    if result['direction'] == 'out':
-        cursor.execute('SELECT bruto FROM transactions WHERE session_id = %s AND direction = %s', (result['session_id'], 'in'))
+    if result[2] == 'out':
+        cursor.execute('SELECT bruto FROM transactions WHERE session_id = %s AND direction = %s', (result[9], 'in'))
         in_weight = cursor.fetchone()
         if in_weight:
-            truck_tara = result['bruto']
-            neto = int(in_weight['bruto']) - int(truck_tara)
+            truck_tara = result[5]
+            neto = int(in_weight[0]) - int(truck_tara)
             response.update({"truckTara": truck_tara, "neto": neto})
         else:
-            response.update({"truckTara": result['bruto'],"session_id": result['session_id'], "neto": "na"})
+            response.update({"truckTara": result[5],"session_id": result[9], "neto": "na"})
 
     return jsonify(response), 200
