9a8c5b9fa931763a886a83404e0db4f6f667be66
MishaTitov <michaeltitov1@gmail.com>
2024-11-20 11:38:37 +0200

    fixed: query datetime format and direction IN ()

--- weight/routes/get_weights.py
+++ weight/routes/get_weights.py
@@ -7,12 +7,14 @@ bp = Blueprint('get_weights', __name__)
 @bp.route('/weight', methods=['GET'])
 def get_weights():
     t1 = request.args.get('from', datetime.datetime.now().strftime('%Y%m%d') + '000000')
+    t1 = datetime.datetime.strptime(t1, '%Y%m%d%H%M%S').strftime("%Y-%m-%d %H:%M:%S")
     t2 = request.args.get('to', datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
+    t2 = datetime.datetime.strptime(t2, '%Y%m%d%H%M%S').strftime("%Y-%m-%d %H:%M:%S")
     filters = request.args.get('filter', 'in,out,none').split(',')
     placeholders = ", ".join(["%s"] * len(filters))
 
     db = get_db()
     cursor = db.cursor()
-    cursor.execute(f'SELECT * FROM transactions WHERE direction IN {placeholders} AND datetime BETWEEN %s AND %s', (*filters, t1, t2))
+    cursor.execute(f'SELECT * FROM transactions WHERE direction IN ({placeholders}) AND datetime BETWEEN %s AND %s', (*filters, t1, t2))
     result = cursor.fetchall()
     return jsonify(result), 200
